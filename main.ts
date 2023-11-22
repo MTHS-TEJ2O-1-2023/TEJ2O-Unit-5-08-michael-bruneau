1e5651b39a3ec3f390e5a7fc9fcd866fff7e5df3
michael-bruneau <144264158+michael-bruneau@users.noreply.github.com>
2023-11-22 12:07:01 -0500

    setup

--- main.ts
+++ main.ts
@@ -1,8 +1,9 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Michael Bruneau
+ * Created on: Nov 2023
+ * This program drives car and stops before hitting wall
 */
 
-basic.showString('Hello, World!')
+// setup
+basic.showIcon(IconNames.Happy)
