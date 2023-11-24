7e77c20900fe8391f89e149603dcff493d807982
michael-bruneau <144264158+michael-bruneau@users.noreply.github.com>
2023-11-24 12:43:25 -0500

    typescript

--- main.ts
+++ main.ts
@@ -13,23 +13,24 @@ basic.showIcon(IconNames.Happy)
 
 while (true) {
   if (input.buttonIsPressed(Button.A) == true) {
+    basic.showIcon(IconNames.Yes)
     while (input.buttonIsPressed(Button.B) == false) {
+      distanceToObject = sonar.ping(
+        DigitalPin.P1,
+        DigitalPin.P2,
+        PingUnit.Centimeters
+      )
       basic.showString(distanceToObject.toString() + ('cm'))
       if (distanceToObject >= 11) {
         basic.showIcon(IconNames.Yes)
-        distanceToObject = sonar.ping(
-          DigitalPin.P1,
-          DigitalPin.P2,
-          PingUnit.Centimeters
-        )
         robotbit.StpCarMove(10, 42)
-        basic.pause(5000)
+        basic.pause(500)
       } else {
         robotbit.StpCarMove(-10, 42)
-        basic.pause(5000)
+        basic.pause(500)
         robotbit.StpCarTurn(90, 42, 125)
-        distanceToObject = 20
-        basic.pause(5000)
+        // distanceToObject = 20
+        basic.pause(500)
       }
     }
   }
