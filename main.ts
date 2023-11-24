dacac2cf242cf445ea52c9a21e56b1d28d46c5e7
michael-bruneau <144264158+michael-bruneau@users.noreply.github.com>
2023-11-24 12:47:50 -0500

    annotations

--- main.ts
+++ main.ts
@@ -11,25 +11,32 @@ let distanceToObject: number = 20
 // setup
 basic.showIcon(IconNames.Happy)
 
+// loop forever
 while (true) {
   if (input.buttonIsPressed(Button.A) == true) {
     basic.showIcon(IconNames.Yes)
-    while (input.buttonIsPressed(Button.B) == false) {
+    
+    // loop everthing
+    while (true) {
+      // check distance
       distanceToObject = sonar.ping(
         DigitalPin.P1,
         DigitalPin.P2,
         PingUnit.Centimeters
       )
+      // show distance
       basic.showString(distanceToObject.toString() + ('cm'))
+
+      // if ditance is greater or equal to 11 move car 10 cm 
       if (distanceToObject >= 11) {
-        basic.showIcon(IconNames.Yes)
         robotbit.StpCarMove(10, 42)
         basic.pause(500)
+        
+        // if 10 cm or less move car 10 cm back and turn right
       } else {
         robotbit.StpCarMove(-10, 42)
         basic.pause(500)
         robotbit.StpCarTurn(90, 42, 125)
-        // distanceToObject = 20
         basic.pause(500)
       }
     }
